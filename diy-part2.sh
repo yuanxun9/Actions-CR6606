95eb67311de65e4ccfaa5c018f9d18feddf967c1
yuanxun9 <49105934+yuanxun9@users.noreply.github.com>
2022-03-25 06:06:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.0.1/192.168.1.30/g' package/base-files/files/bin/config_generate
+ZZZ="package/lean/default-settings/files/zzz-default-settings"
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
+sed -i "s/OpenWrt /JunYi compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
