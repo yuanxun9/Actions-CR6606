7b4ed95662c0e6fd4a7b3c2fc3c5aee25a5bd6b7
yuanxun9 <49105934+yuanxun9@users.noreply.github.com>
2022-03-25 15:42:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git clone https://github.com/thinktip/luci-theme-neobird.git
