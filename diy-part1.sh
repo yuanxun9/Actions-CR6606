58535edcb0db263534f7d25aba183eee10726e25
yuanxun9 <49105934+yuanxun9@users.noreply.github.com>
2022-03-25 15:42:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+git clone https://github.com/thinktip/luci-theme-neobird.git
