67db5058ec271d90664886f2dfa1bfc8c637c739
yuanxun9 <49105934+yuanxun9@users.noreply.github.com>
2022-04-05 06:02:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,11 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-sed -i 's/192.168.0.1/192.168.1.30/g' package/base-files/files/bin/config_generate
+# Modify default
 sed -i 's/luci-theme-bootstrap/Luci-theme-neobird/g' feeds/luci/collections/luci/Makefile
-
-ZZZ="package/lean/default-settings/files/zzz-default-settings"
-
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
-sed -i "s/OpenWrt /JunYi compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
